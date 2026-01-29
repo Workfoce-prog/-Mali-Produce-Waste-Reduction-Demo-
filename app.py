3df0199328472acbd8b50b0ede300618d17cedb8
Workfoce-prog <ziyackcoulibaly@gmail.com>
2026-01-29 11:35:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 
+import streamlit as st
+import pandas as pd
+from datetime import date
+
 from settings import APP_TITLE, DEFAULT_LANGUAGE
-from db import load_db, add_record, delete_record
+from db import load_db, add_record
 from matching import match_supply_demand
 from impact import compute_impact
 from i18n import I18N
-from google_sync import push_to_google
+
 
 st.set_page_config(page_title=APP_TITLE, layout="wide")
 st.title(APP_TITLE)
